3a865b3753c05bb2487e57ddbaab4664b77d6a4d
Andrea Hawksley <hawksley@gmail.com>
2015-12-17 16:40:18 -0800

    set up object, cry at shader

--- js/webVR.js
+++ js/webVR.js
@@ -1,39 +1,127 @@
-// Setup three.js WebGL renderer
-var renderer = new THREE.WebGLRenderer( { antialias: true } );
+"use strict";
 
-// Append the canvas element created by the renderer to document body element.
-document.body.appendChild( renderer.domElement );
+var materialBase, renderer, scene, camera, controls, effect, projector;
 
-//Create a three.js scene
-var scene = new THREE.Scene();
+function init() {
+  // Setup three.js WebGL renderer
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
 
-//Create a three.js camera
-var camera = new THREE.PerspectiveCamera( 110, window.innerWidth / window.innerHeight, 2, 10000 );
-scene.add(camera);
+  // Append the canvas element created by the renderer to document body element.
+  document.body.appendChild( renderer.domElement );
+  renderer.setClearColor( 0x000000, 1 );
 
-//Apply VR headset positional data to camera.
-var controls = new THREE.VRControls( camera );
+  //Create a three.js scene
+  scene = new THREE.Scene();
 
-//Apply VR stereo rendering to renderer
-var effect = new THREE.VREffect( renderer );
-effect.setSize( window.innerWidth, window.innerHeight );
+  //Create a three.js camera
+  camera = new THREE.PerspectiveCamera( 110, window.innerWidth / window.innerHeight, 2, 10000 );
+  scene.add(camera);
 
+  //Apply VR headset positional data to camera.
+  controls = new THREE.VRControls( camera );
 
-/************* TODO: Generate Your VR Scene Below *********************/
+  //Apply VR stereo rendering to renderer
+  effect = new THREE.VREffect( renderer );
+  effect.setSize( window.innerWidth, window.innerHeight );
 
+  effect.render(scene, camera);
 
-/*
-TODO: Create, position, and add 3d objects here
-*/
+  // initialize object to perform world/screen calculations
+	projector = new THREE.Projector();
 
+  // material for the cells is a shader
+  materialBase = new THREE.ShaderMaterial({
+    // these are the parameters for the shader
+    uniforms: {
+      time: { // global time
+        type: "f",
+        value: 0.0
+      },
+      quatPerCell: {  // quaternion that moves cells into 4-space, set once per cell
+        type: "v4",
+        value: new THREE.Vector4( 0, 0, 0, 0 )
+      },
+      mousePos: {
+        type: "v2",
+        value: new THREE.Vector2(0,0)
+      },
+      travelDir: {
+        type: "v4",
+        value: new THREE.Vector4( 0, 0, 0, 0 )
+      },
+      colourDir: {
+        type: "v4",
+        value: new THREE.Vector4( 0, 0, 0, 0 )
+      },
+      HopfColorMatrix: {
+        type: "m4",
+        value: new THREE.Matrix4().set( 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 )
+      },
+      moveQuat: {
+        type: "v4",
+        value: new THREE.Vector4( 0, 0, 0, 1 )
+      },
+      rotMatrix: {
+        type: "m3",
+        value: new THREE.Matrix3().set( 0, 0, 0, 0, 0, 0, 0, 0, 0 )
+      },
+      modelScale: {
+        type: "f",
+        value: 1.0
+      },
+      objectScale: {
+        type: "f",
+        value: 1.0
+      },
+      objectPosn: {
+        type: "v4",
+        value: new THREE.Vector4( 0, 0, -2, 0 )
+      },
+      userPosn: {
+        type: "v4",
+        value: new THREE.Vector4( 0, 0, 0, 0 )
+      }
+    },
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent
+  });
+  materialBase.side = THREE.FrontSide;
+
+  loadStuff();
+}
+
+var modelFileName = "media/hypercube_2_skeleton.obj";
+var resultingObj;
+function loadStuff() {
+  var manager = new THREE.LoadingManager();
+  var loader = new THREE.OBJLoader(manager);
+  loader.load(modelFileName, function (object) {
+    resultingObj = object.clone();
+
+    resultingObj.traverse(function (child) {
+      if (child instanceof THREE.Mesh) {
+        var material = new THREE.MeshNormalMaterial();
+        material.side = THREE.DoubleSide;
+
+        child.material = materialBase.clone();
+
+        // child.material = material;
+        // child.position.z = -2;
+
+        child.frustumCulled = false;
+      }
+    });
+
+    scene.add(resultingObj);
+  });
+}
+
+init();
 
 /*
 Request animation frame loop function
 */
 function animate() {
-  // TODO: Apply any desired changes for the next frame here.
-
-
   //Update VR headset position and apply to camera.
   controls.update();
 
@@ -43,12 +131,6 @@ function animate() {
 }
 
 animate();	// Kick off animation loop
-
-
-
-/***************** TODO: Generate Your VR Scene Above *****************/
-
-
 
 /*
 Listen for click event to enter full-screen mode.
@@ -80,4 +162,4 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   effect.setSize( window.innerWidth, window.innerHeight );
 }
-window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener('resize', onWindowResize, false);
